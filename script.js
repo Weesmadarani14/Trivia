a422aede1e26225f68b347592fbebf50cbd56985
Wissam Madarani <wissammadarani@gmail.com>
2024-01-25 10:49:53 -0500

    Javascript

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// check the answers to the code
 document.addEventListener('DOMContentLoaded', function() {
-    
+    let corrects = document.querySelectorAll('.correct');
+    for (let i = 0; i < corrects.length; i++) 
+    {
+        corrects[i].addEventListener('click', function () {
+            corrects[i].style.backgroundColor = 'Green';
+            corrects[i].parentElement('.feedback').innerHTML = "CORRECT!!! :)";
+        })
+    }
 });
