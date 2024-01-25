c140b67d26b0121c11132a3f998b013277f72872
Wissam Madarani <wissammadarani@gmail.com>
2024-01-25 10:57:42 -0500

    Javascript

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
         corrects[i].addEventListener('click', function () {
             corrects[i].style.backgroundColor = 'Green';
             corrects[i].parentElement('.feedback').innerHTML = "CORRECT!!! :)";
-        })
+        });
     }
+
+    let incorrects = document.querySelectorAll('.incorrect', function() {
+        for (let i = 0; i < incorrects.length; i++)
+        {
+            incorrects[i].addEventListener('click', function() {
+                incorrects[i].style.backgroundColor = 'Red';
+                incorrects[i].parentElement('.feedback').innerHTML = "INCORRECT :(";
+            });
+        }
+    });
 });
